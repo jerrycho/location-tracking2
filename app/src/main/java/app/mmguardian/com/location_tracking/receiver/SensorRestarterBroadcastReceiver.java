c6175405152ce97fac0389126fe2668e917a2c2a
jerry.cho <jerry.cho@goxip.com>
2018-06-11 22:24:44 +0800

    OLD service version 1

--- app/src/main/java/app/mmguardian/com/location_tracking/receiver/SensorRestarterBroadcastReceiver.java
+++ app/src/main/java/app/mmguardian/com/location_tracking/receiver/SensorRestarterBroadcastReceiver.java
@@ -4,12 +4,19 @@ package app.mmguardian.com.location_tracking.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import app.mmguardian.com.location_tracking.service.TrackingService;
+import app.mmguardian.com.location_tracking.service.TrackingService2;
 
 public class SensorRestarterBroadcastReceiver extends BroadcastReceiver{
+
+    public final static String TAG = "location_tracking";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         //start the service
-        //context.startService(new Intent(context, SensorService.class));
+        Log.d(TAG,"onReceive SensorRestarterBroadcastReceiver");
+        context.startService(new Intent(context, TrackingService2.class));
     }
 }
--- app/src/main/java/app/mmguardian/com/location_tracking/service/TrackingService2.java
+++ app/src/main/java/app/mmguardian/com/location_tracking/service/TrackingService2.java
@@ -1,7 +1,10 @@
 package app.mmguardian.com.location_tracking.service;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
@@ -13,16 +16,16 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import app.mmguardian.com.Constants;
 import app.mmguardian.com.location_tracking.bus.ServiceEventConnectedEvent;
 import app.mmguardian.com.location_tracking.utils.LocationTracking;
 
 
-public class TrackingService extends Service {
+public class TrackingService2 extends Service {
 
     public final static String TAG = "location_tracking";
+    public static final String ACTION="app.mmguardian.com.location_tracking.StartTimer";
 
     ScheduledExecutorService mScheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
@@ -31,21 +34,30 @@ public class TrackingService extends Service {
 
     private boolean isStartedTimer = false;
 
-    IBinder mBinder = new LocalBinder();
-
-    public class LocalBinder extends Binder {
-        public TrackingService getServerInstance() {
-            return TrackingService.this;
-        }
-    }
+    BroadcastReceiver startTimerReceiver;
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return mBinder;
+        return null;
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        final IntentFilter theFilter = new IntentFilter();
+        theFilter.addAction(ACTION);
+        this.startTimerReceiver = new BroadcastReceiver() {
 
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                doStartTimer();
+            }
+        };
+        // Registers the receiver so that your service will listen for
+        // broadcasts
+        this.registerReceiver(this.startTimerReceiver, theFilter);
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -56,13 +68,14 @@ public class TrackingService extends Service {
             mScheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
         }
 
+        doStartTimer();
+
         return START_STICKY;
     }
 
     public void doStartTimer() {
         if (!isStartedTimer) {
-            EventBus.getDefault().post(new ServiceEventConnectedEvent());
-            LocationTracking mLocationTracking = new LocationTracking(TrackingService.this);
+            LocationTracking mLocationTracking = new LocationTracking(TrackingService2.this);
             mLocationTracking.doStartTimer();
 
             isStartedTimer = true;
@@ -84,7 +97,7 @@ public class TrackingService extends Service {
         mTimerTask = new TimerTask() {
             @Override
             public void run() {
-                LocationTracking mLocationTracking = new LocationTracking(TrackingService.this);
+                LocationTracking mLocationTracking = new LocationTracking(TrackingService2.this);
                 mLocationTracking.doStartTimer();
             }
         };
@@ -95,6 +108,7 @@ public class TrackingService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
+        this.unregisterReceiver(this.startTimerReceiver);
         Intent broadcastIntent = new Intent("app.mmguardian.com.location_tracking.RestartSensor");
         sendBroadcast(broadcastIntent);
         doStopTimer();
